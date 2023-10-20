21e9d93491570fd75e100229168ccdad89ab38d6
Raushan Kumar <raushan@ineuron.ai>
2023-10-20 13:45:09 +0530

    added socket.io

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv';
+import http from 'node:http';
+import { Server } from 'socket.io';
+
 dotenv.config();
 
 const app = express();
+const httpServer = http.createServer(app);
+const io = new Server(httpServer);
 
-
-export default app;
+export default httpServer;
 
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import app from "./app";
 import { connectToDB } from "./config/db";
 
 app.listen(8080, ()=>{
-    console.log(`Server running at http://localhost:8080`);
+    console.log('Server running at http://localhost:8080');
     connectToDB();
 });
